86d7e7820db3b0f30245f289890763d62fd62842
ConorGoold <conor.goold@nmbu.no>
2017-05-17 15:38:54 +0200

    commit

---
+++ BradleyTerry_withCovariate.stan
@@ -0,0 +1,51 @@
+data{
+int<lower=0> N_dyads;       // number of dyads
+int<lower=0> N_ids;         // number of individuals
+int<lower=0> P;              // number of covariates
+int<lower=0> ind1[N_dyads];   // first individual in the dyad
+int<lower=0> ind2[N_dyads];   // second individual in the dyad
+int<lower=0> win1[N_dyads];   // number of wins by the first individual
+int<lower=0> win2[N_dyads];   // number of wins by the second individual
+row_vector[P] X[N_ids];       // covariates; 0 must be meaningful!
+}
+
+transformed data{
+int<lower=0> n_ints[N_dyads];     // total interactions between each dyad
+for(n in 1:N_dyads) {
+  n_ints[n] = win1[n] + win2[n];
+  }
+}
+
+parameters{
+vector[N_ids] d_raw;                  // latent dominance value parameter
+//real d_alpha;
+vector[P] Beta;                   // beta coefficients
+real<lower=0> sigma;        // standard deviation of dominance values
+}
+
+transformed parameters{
+vector[N_ids] d;
+for(j in 1:N_ids ) {
+  d[j] = (X[j] * Beta) + d_raw[j] * sigma;     // non-centered parameterisation
+  }
+}
+
+model{
+vector[N_dyads] p;        // local parameter
+
+Beta ~ normal(0, 1);
+sigma ~ cauchy(0, 2);
+//d_alpha ~ normal(0, 1);
+
+d_raw ~ normal( 0, 1 );
+
+for( n in 1:N_dyads) {
+
+    // probability of winning = the difference in dominance values between individuals
+    // uses the logistic/inverse logit function to transform to probability
+
+  p[n] = inv_logit( d[ ind1[n] ] - d[ ind2[n] ] );
+ }
+
+  win1 ~ binomial( n_ints , p );    // vectorised
+}
--- R_scipt.R
+++ R_scipt.R
@@ -1,6 +1,6 @@
 library(rstsan)
 library(rethinking)
-library(reshape2)
+library(ggplot2)
 
 setwd("~/Documents/PhD_NMBU/BradleyTerry_dominance")
 
@@ -61,4 +61,77 @@ rownames(dominance_orders) <- 1:length(unique_orders)
 
 ## NB: The results replicate Adams (2005) partly, but simulation variance in the MCMC chain
 ## means that the lower probability dominance orders are likely to change. 
-## This is not a fully Bayesian approach!
+## This is not really a fully Bayesian approach. 
+
+#==========================================================
+# Fit the second Bradley-Terry model, using data from Table 2.
+
+# This data set is of wins and losses between 20 female bighorn sheep, where the
+# interest is in relating dominance to age.
+#==========================================================
+
+d2 <- read.csv("Adams2.csv")
+d2_ages <- read.csv("Adams2_ages.csv")
+
+d2$ind1 <- rep(1:length(unique(d2$ind1)),
+                     sort(unlist(lapply(split(d2, d2$ind1), 
+                         function(z) nrow(z)) ) , decreasing = TRUE))
+
+ind2_new <- rep(list(list()), length(unique(d2$ind2)))
+for(i in 1:19){
+  ind2_new[[i]] <- c(unique(d2$ind1),20)[-c(1:i)]
+}
+
+d2$ind2 <- unlist(ind2_new)
+
+d2_ages$ageZ <- (d2_ages$age - mean(d2_ages$age))/sd(d2_ages$age)
+X_mat = as.matrix(d2_ages$ageZ)
+
+stan_data_list <- list(N_dyads = nrow(d2), N_ids = length(unique(d2$ind1))+1,
+                       ind1 = d2$ind1, ind2 = d2$ind2, 
+                       win1 = d2$win1, win2 = d2$win2, 
+                       X = X_mat, P = ncol(X_mat) ) 
+
+fit_stan2 <- stan(file = "BradleyTerry_withCovariate.stan", 
+                  data = stan_data_list, 
+                  chains = 4, cores = 4, iter = 5000, warmup = 2500
+)
+
+print(fit_stan2)
+
+p_samples <- as.data.frame(fit_stan2)
+
+#=========== Re-create figure 2 ===============================================
+
+age_comb <- d2_ages$ageZ
+pred <- sapply(age_comb, 
+               function(z) p_samples$`Beta[1]` * z )
+pred_mu <- apply(pred, 2, mean)
+pred_hdi <- apply(pred, 2, function(z) HPDI(z,0.89) )
+
+plotting_df <- data.frame(id = 1:20, 
+                          age = d2_ages$age,
+                          ageZ = d2_ages$ageZ,
+                          mu = pred_mu, 
+                          hdi_low = pred_hdi[1,], hdi_high = pred_hdi[2,],
+                          dom_mu = as.vector(apply(p_samples[,grep("d",colnames(p_samples))][21:40],
+                                                   2, mean ) ),
+                          dom_low = as.vector(apply(p_samples[,grep("d",colnames(p_samples))][21:40],
+                                                   2, function(z) HPDI(z) )[1,] ),
+                          dom_high = as.vector(apply(p_samples[,grep("d",colnames(p_samples))][21:40],
+                                                    2, function(z) HPDI(z) )[2,] )
+                          )
+
+ggplot(plotting_df, aes(age, mu)) + 
+  geom_line() + 
+  geom_point(aes(age, dom_mu, group=id), position = position_dodge(0.5)) + 
+  geom_errorbar(aes(ymin=dom_low, ymax=dom_high, group=id), 
+                width=0, position = position_dodge(0.5)) + 
+  labs(x = "age", y="dominance value") + 
+  theme_bw() + 
+  theme(panel.grid = element_blank())
+
+
+# NB: The dominance values and regression line seem to lie over a smaller range than 
+## in Adams (2005). This is probably (not tested) a result of using regularising priors, rather than the vague priors used in BUGS. 
+
